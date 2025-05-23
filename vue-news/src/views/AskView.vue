030a70118d91f772b941a6e4862f9a55e9d717ce
윤제영 <goods0103@naver.com>
2025-05-23 23:10:15 +0900

    section7 commit

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>ask</div>
+  <div>
+    <div v-for="ask in asks">{{ ask }}</div>
+  </div>
 </template>
 
 <script>
+import { fetchAskList } from "../apis/index.js";
 export default {
-
-}
+  data() {
+    return {
+      asks: [],
+    };
+  },
+  created() {
+    fetchAskList()
+      .then((response) => (this.asks = response.data))
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>jobs</div>
+  <div>
+    <div v-for="job in jobs">{{ job }}</div>
+  </div>
 </template>
 
 <script>
-export default {
+import { fetchJobsList } from "../apis/index.js";
 
-}
+export default {
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  created() {
+    fetchJobsList()
+      .then((response) => (this.jobs = response.data))
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
