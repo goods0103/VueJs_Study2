ab5046536bc80a7ba2e9142e72cdd9a03df00506
윤제영 <goods0103@naver.com>
2025-05-25 18:21:35 +0900

    section12

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,34 +1,17 @@
 <template>
   <div>
-    <section>
-      <!-- 질문상세영역 -->
-      <div class="user-container">
-        <div>
-          <i class="fas fa-user"></i>
-        </div>
-        <div class="user-decription">
-          <router-link :to="`/user/${fetchedItem.user}`">
-            {{ fetchedItem.user }}
-          </router-link>
-          <div class="time">
-            {{ fetchedItem.time_ago }}
-          </div>
-        </div>
-      </div>
-      <h2>{{ fetchedItem.title }}</h2>
-    </section>
-    <section>
-      <!-- 질문 댓글  -->
-      <div v-html="fetchedItem.content"></div>
-    </section>
-    <!-- <p>title: {{ fetchedItem.title }}</p>
-    <div>content: {{ fetchedItem.content }}</div> -->
+    <user-profile></user-profile>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import UserProfile from "../components/UserProfile.vue";
+
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     // itemInfo() {
     //   return this.$store.state.item;
@@ -42,20 +25,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.user-container {
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-}
-.fa-user {
-  font-size: 2.5rem;
-}
-.user-decription {
-  padding-left: 8px;
-}
-.time {
-  font-size: 0.7rem;
-}
-</style>
