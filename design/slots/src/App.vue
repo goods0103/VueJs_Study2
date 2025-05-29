09875b627662fa6e69306261fccd03a0e699d404
=윤제영 <goods0103@naver.com>
2025-05-29 22:54:43 +0900

    section18 commit

---
+++ design/slots/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <item>
+        아이템1
+      </item>
+      <item> 아이템2 <button>click me</button> </item>
+      <item>
+        아이템3
+      </item>
+      <img
+        src="./assets/endgame.png"
+        alt="어벤져스 엔드게임"
+        width="50px"
+        height="50px"
+      />
+      <item>
+        <div style="color: blue;">아이템4</div>
+      </item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  components: {
+    Item,
+  },
+};
+</script>
